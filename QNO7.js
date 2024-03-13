bc208e48ed7a49f3285e484f00f117283594d9db
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 00:12:16 +0500

    Add files via upload

---
+++ QNO7.js
@@ -0,0 +1,19 @@
+"use strict";
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+Object.defineProperty(exports, "__esModule", { value: true });
+// NO1)ADDITION +
+let addition1 = 5;
+let addition2 = 3;
+console.log(addition1 + addition2);
+// 2)SUBTRACTION -
+let subtraction1 = 16;
+let subtraction2 = 8;
+console.log(subtraction1 - subtraction2);
+// 3)MULTIPLICATION
+let multiplication1 = 2;
+let multiplication2 = 4;
+console.log(multiplication1 * multiplication2);
+// 4)DIVISION
+let division1 = 64;
+let division2 = 8;
+console.log(division1 / division2);
---
+++ QNO7.ts
@@ -0,0 +1,22 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+// NO1)ADDITION +
+let addition1: number = 5;
+let addition2: number = 3;
+console.log(addition1 + addition2);
+
+// 2)SUBTRACTION -
+let subtraction1: number = 16;
+let subtraction2: number = 8;
+console.log(subtraction1 - subtraction2);
+
+// 3)MULTIPLICATION
+let multiplication1: number = 2;
+let multiplication2: number = 4;
+console.log(multiplication1 * multiplication2);
+
+// 4)DIVISION
+
+let division1: number = 64;
+let division2: number = 8;
+console.log(division1 / division2);
